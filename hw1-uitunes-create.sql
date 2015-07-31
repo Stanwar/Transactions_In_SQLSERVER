65efe3797e9c4cd4b2f1c5f1d6fad2cbe156b41e
RRC <RRC@10-110-221-216.vlan911.client.wireless.uic.edu>
2015-07-31 14:03:19 -0500

    first commit

---
+++ hw1-uitunes-create.sql
@@ -0,0 +1,144 @@
+﻿
+--
+-- SQL Script to Create Uitunes database
+-- Sharad Tanwar ( stanwa2)
+-- U.Illinois , Chicago
+-- CS 480, Summer 2015
+-- HomeWork 1
+--
+DROP TABLE PurchaseDetails;
+DROP TABLE Purchase;
+DROP TABLE PlaylistDetails;
+DROP TABLE Reviews;
+DROP TABLE Playlists;
+DROP TABLE SongAlbumDetails;
+DROP TABLE SongArtistDetails;
+DROP TABLE Songs;
+DROP TABLE Albums;
+DROP TABLE Artists;
+DROP TABLE Genres;
+DROP TABLE Users;
+-- 
+-- 
+-- 
+
+CREATE TABLE Genres (
+   GenreID   SMALLINT IDENTITY(1,1) PRIMARY KEY,  -- IDENTITY = auto-increment, start at 1, by 1
+   GenreName NVARCHAR(32) NOT NULL                -- Unicode string, variable length, max 32
+);
+
+
+CREATE TABLE Songs (
+   SongID   INT IDENTITY(1,1) PRIMARY KEY,  -- IDENTITY = auto-increment, start at 1, by 1
+   SongName NVARCHAR(240) NOT NULL,         -- Unicode string, variable length, max 128
+   YearRel  SMALLINT NOT NULL,                       -- e.g. 2014
+   Price	SMALLMONEY NOT NULL CHECK( Price >=0),
+   Duration TIME,                          -- hh:mm:ss, e.g. 00:03:30
+   Discount INT
+);
+
+--
+CREATE TABLE Albums(
+	AlbumID		INT IDENTITY(1,1) PRIMARY KEY,
+	AlbumName	NVARCHAR(240) NOT NULL,
+	YearRel		SMALLINT NOT NULL,
+	Price		SMALLMONEY NOT NULL
+);
+
+-- Main Artists Table 
+CREATE TABLE Artists (
+   ArtistID   INT IDENTITY(1,1) PRIMARY KEY,  -- IDENTITY = auto-increment, start at 1, by 1
+   ArtistName NVARCHAR(64) NOT NULL,          -- Unicode string, variable length, max 64
+   YearBorn   SMALLINT		NOT NULL     			-- Year in which the Artist was born 
+);
+
+-- Main Users Table 
+
+CREATE TABLE Users (
+	UserID				INT IDENTITY(1,1) PRIMARY KEY,
+	FirstName			NVARCHAR(240) NOT NULL,
+	LastName			NVARCHAR(240) NOT NULL,
+	Email				NVARCHAR(240) NOT NULL UNIQUE, -- Email has to be unique for every user
+	Pssword				NVARCHAR(240) NOT NULL,
+	AccountBalance		INT	         NOT NULL
+);
+-- Details table for Songs
+CREATE TABLE SongArtistDetails (
+   SongID     INT NOT NULL FOREIGN KEY REFERENCES Songs(SongID),
+   ArtistID   INT NOT NULL FOREIGN KEY REFERENCES Artists(ArtistID),
+   ArtistRole NVARCHAR(50) NOT NULL,               -- Lead/Author/Feat
+   GenreID    SMALLINT FOREIGN KEY REFERENCES Genres(GenreID), 	
+   PRIMARY KEY(SongID, ArtistID)
+);
+
+-- Details table for Songs
+CREATE TABLE SongAlbumDetails (
+   SongID     INT NOT NULL FOREIGN KEY REFERENCES Songs(SongID),
+   AlbumID   INT NOT NULL FOREIGN KEY REFERENCES Albums(AlbumID),
+   GenreID    SMALLINT FOREIGN KEY REFERENCES Genres(GenreID), 	
+   PRIMARY KEY(SongID, AlbumID)
+);
+
+
+-- Playlists
+CREATE TABLE Playlists(
+	PlaylistID INT IDENTITY(1,1) PRIMARY KEY,					-- ID for Playlist 
+	PlaylistName NVARCHAR(240) NOT NULL,						-- Playlist Name 
+	UserID INT NOT NULL FOREIGN KEY REFERENCES Users(UserID)	-- UserID References to Users Table 
+);
+
+-- PlayListDetails 
+CREATE TABLE PlaylistDetails(
+	PlaylistID INT FOREIGN KEY REFERENCES Playlists(PlaylistID),
+	ItemType NVARCHAR(15) NOT NULL, -- SONG/ ALBUM
+	ItemID INT NOT NULL,
+	AddDate DATE,
+	Visible BIT NOT NULL, -- 0 = NOT VISIBLE, 1 = VISIBLE 
+	PRIMARY KEY (PlaylistID, ItemID, ItemType) 
+);
+
+-- Reviews Table 
+CREATE TABLE Reviews(
+	ReviewId INT IDENTITY(1,1) PRIMARY KEY,						-- ReviewID
+	AlbumID INT FOREIGN KEY REFERENCES Albums(AlbumID) ,		-- AlbumID
+	SongID INT FOREIGN KEY REFERENCES Songs(SongID),			-- SongID
+	PlaylistID INT FOREIGN KEY REFERENCES Playlists(PlaylistID), -- Playlist ID 
+	ReviewType NVARCHAR(15) NOT NULL,									-- ALBUM , SONG , PLAYLIST 
+	UserID INT NOT NULL FOREIGN KEY REFERENCES Users(UserID) ,			-- UserID 
+	Rating TINYINT NOT NULL CHECK (Rating>=1 AND Rating<=5) ,	-- Rating 1-5
+	COMMENT NVARCHAR(4000)										-- Optional Comment 
+);
+
+-- Main Purchase Table 
+CREATE TABLE Purchase(
+	TransactionID  INT IDENTITY(1,1) PRIMARY KEY,			 -- Purchase Transaction ID 
+	UserID	INT NOT NULL FOREIGN KEY REFERENCES Users(UserID), -- The purchaser
+	NumberOfItems  INT CHECK (NumberOfItems >=0),										 -- No of items being brought
+	Amount		   SMALLMONEY NOT NULL,										 -- Amount of the Purchase
+	DateOfPurchase DATE	NOT NULL									 -- Date of Purchase
+
+);
+
+-- Purchase Details Table which details the purchases in a single transaction
+CREATE TABLE PurchaseDetails(
+	TransactionID INT FOREIGN KEY REFERENCES Purchase(TransactionID),
+	PurchaseType NVARCHAR(15) NOT NULL,					-- Album/ Song
+	PurchaseItemID INT NOT NULL,					-- AlbumID /SongID
+	ItemAmount SMALLMONEY NOT NULL,                       
+	PRIMARY KEY (TransactionID, PurchaseType, PurchaseItemID) -- Primary Key 
+);
+
+-- Indexes
+CREATE INDEX Review_User_Indx ON Reviews(ReviewID,UserID);
+
+CREATE INDEX Review_User_Album_Indx ON Reviews(UserID,AlbumID);
+
+CREATE INDEX Review_User_Song_Indx ON Reviews(UserID,SongID);
+
+CREATE INDEX Albums_Name_Indx ON Albums(AlbumName);
+
+CREATE INDEX Artists_Name_Indx ON Artists(ArtistName);
+
+CREATE INDEX Song_Name_Indx ON Songs(SongName);
+
+CREATE INDEX Users_Name_Indx ON Users(UserID);
---
+++ hw1-uitunes-insert.sql
@@ -0,0 +1,84 @@
+﻿
+--
+-- SQL Script to Insert Records in Uitunes database
+-- Sharad Tanwar ( stanwa2)
+-- U.Illinois , Chicago
+-- CS 480, Summer 2015
+-- HomeWork 1
+--
+
+SET IDENTITY_INSERT [dbo].[Albums] ON
+INSERT INTO [dbo].[Albums] ([AlbumID], [AlbumName], [YearRel], [Price]) VALUES (1, N'8 Mile', 2014, CAST(5.0000 AS SmallMoney))
+INSERT INTO [dbo].[Albums] ([AlbumID], [AlbumName], [YearRel], [Price]) VALUES (2, N'1989', 2014, CAST(10.0000 AS SmallMoney))
+INSERT INTO [dbo].[Albums] ([AlbumID], [AlbumName], [YearRel], [Price]) VALUES (5, N'Living Dead Girl', 2010, CAST(2.0000 AS SmallMoney))
+INSERT INTO [dbo].[Albums] ([AlbumID], [AlbumName], [YearRel], [Price]) VALUES (6, N'Citizen Cope', 2010, CAST(10.0000 AS SmallMoney))
+SET IDENTITY_INSERT [dbo].[Albums] OFF
+
+SET IDENTITY_INSERT [dbo].[Artists] ON
+INSERT INTO [dbo].[Artists] ([ArtistID], [ArtistName], [YearBorn]) VALUES (1, N'Citizen Cope', 1970)
+INSERT INTO [dbo].[Artists] ([ArtistID], [ArtistName], [YearBorn]) VALUES (2, N'Eminem', 1973)
+INSERT INTO [dbo].[Artists] ([ArtistID], [ArtistName], [YearBorn]) VALUES (3, N'Led Zeppelin', 1967)
+INSERT INTO [dbo].[Artists] ([ArtistID], [ArtistName], [YearBorn]) VALUES (4, N'Pink Floyd', 1955)
+SET IDENTITY_INSERT [dbo].[Artists] OFF
+
+SET IDENTITY_INSERT [dbo].[Genres] ON
+INSERT INTO [dbo].[Genres] ([GenreID], [GenreName]) VALUES (1, N'Rap')
+INSERT INTO [dbo].[Genres] ([GenreID], [GenreName]) VALUES (2, N'Hip Hop')
+INSERT INTO [dbo].[Genres] ([GenreID], [GenreName]) VALUES (3, N'Rock')
+INSERT INTO [dbo].[Genres] ([GenreID], [GenreName]) VALUES (4, N'Heavy Metal')
+INSERT INTO [dbo].[Genres] ([GenreID], [GenreName]) VALUES (5, N'Fruity')
+SET IDENTITY_INSERT [dbo].[Genres] OFF
+
+SET IDENTITY_INSERT [dbo].[Users] ON
+INSERT INTO [dbo].[Users] ([UserID], [FirstName], [LastName], [Email], [Pssword], [AccountBalance]) VALUES (1, N'Sharad', N'Tanwar', N'sharad_tanwar@outlook.com', N'sharad', 1000)
+INSERT INTO [dbo].[Users] ([UserID], [FirstName], [LastName], [Email], [Pssword], [AccountBalance]) VALUES (3, N'Max', N'Behran', N'max_behran@outlook.com', N'max', 500)
+INSERT INTO [dbo].[Users] ([UserID], [FirstName], [LastName], [Email], [Pssword], [AccountBalance]) VALUES (5, N'Prateek', N'Garg', N'pkgarg@outlook.com', N'horny', 300)
+SET IDENTITY_INSERT [dbo].[Users] OFF
+
+SET IDENTITY_INSERT [dbo].[Songs] ON
+INSERT INTO [dbo].[Songs] ([SongID], [SongName], [YearRel], [Price], [Duration], [Discount]) VALUES (1, N'Sideways', 2014, CAST(2.0000 AS SmallMoney), N'00:04:00', 3)
+INSERT INTO [dbo].[Songs] ([SongID], [SongName], [YearRel], [Price], [Duration], [Discount]) VALUES (2, N'Holdin On', 2014, CAST(3.0000 AS SmallMoney), N'00:03:48', 3)
+INSERT INTO [dbo].[Songs] ([SongID], [SongName], [YearRel], [Price], [Duration], [Discount]) VALUES (3, N'Lose Yourself', 2002, CAST(3.0000 AS SmallMoney), N'00:03:12', 0)
+INSERT INTO [dbo].[Songs] ([SongID], [SongName], [YearRel], [Price], [Duration], [Discount]) VALUES (4, N'Blank Space', 2015, CAST(3.0000 AS SmallMoney), N'00:03:58', 0)
+SET IDENTITY_INSERT [dbo].[Songs] OFF
+
+INSERT INTO [dbo].[SongArtistDetails] ([SongID], [ArtistID], [ArtistRole], [GenreID]) VALUES (1, 1, N'LEAD', 1);
+INSERT INTO [dbo].[SongArtistDetails] ([SongID], [ArtistID], [ArtistRole], [GenreID]) VALUES (1, 2, N'FEAT', 1);
+INSERT INTO [dbo].[SongArtistDetails] ([SongID], [ArtistID], [ArtistRole], [GenreID]) VALUES (2, 1, N'LEAD', 2);
+INSERT INTO [dbo].[SongArtistDetails] ([SongID], [ArtistID], [ArtistRole], [GenreID]) VALUES (2, 2, N'FEAT', 2);
+
+INSERT INTO [dbo].[SongAlbumDetails] ([SongID], [AlbumID], [GenreID]) VALUES (1, 1, 1)
+INSERT INTO [dbo].[SongAlbumDetails] ([SongID], [AlbumID], [GenreID]) VALUES (2, 1, 1)
+INSERT INTO [dbo].[SongAlbumDetails] ([SongID], [AlbumID], [GenreID]) VALUES (3, 1, 1)
+INSERT INTO [dbo].[SongAlbumDetails] ([SongID], [AlbumID], [GenreID]) VALUES (2, 2, 2)
+INSERT INTO [dbo].[SongAlbumDetails] ([SongID], [AlbumID], [GenreID]) VALUES (3, 2, 2)
+INSERT INTO [dbo].[SongAlbumDetails] ([SongID], [AlbumID], [GenreID]) VALUES (1, 2, 2)
+
+
+SET IDENTITY_INSERT [dbo].[Playlists] ON
+INSERT INTO [dbo].[Playlists] ([PlaylistID], [PlaylistName], [UserID]) VALUES (1, N'Chill', 1)
+INSERT INTO [dbo].[Playlists] ([PlaylistID], [PlaylistName], [UserID]) VALUES (4, N'Melody', 5)
+SET IDENTITY_INSERT [dbo].[Playlists] OFF
+
+
+INSERT INTO [dbo].[PlaylistDetails] ([PlaylistID], [ItemType], [ItemID], [AddDate], [Visible]) VALUES (1, N'ALBUM', 1, N'2014-11-12', 0)
+INSERT INTO [dbo].[PlaylistDetails] ([PlaylistID], [ItemType], [ItemID], [AddDate], [Visible]) VALUES (1, N'SONG', 1, N'2013-05-05', 1)
+INSERT INTO [dbo].[PlaylistDetails] ([PlaylistID], [ItemType], [ItemID], [AddDate], [Visible]) VALUES (1, N'SONG', 2, N'2013-04-04', 1)
+
+SET IDENTITY_INSERT [dbo].[Reviews] ON
+INSERT INTO [dbo].[Reviews] ([ReviewId], [AlbumID], [SongID], [PlaylistID], [ReviewType], [UserID], [Rating], [COMMENT]) VALUES (1, 1, NULL, NULL, N'ALBUM', 1, 3, N'This isnt bad')
+INSERT INTO [dbo].[Reviews] ([ReviewId], [AlbumID], [SongID], [PlaylistID], [ReviewType], [UserID], [Rating], [COMMENT]) VALUES (3, 1, NULL, NULL, N'ALBUM', 5, 4, N'Excellant')
+INSERT INTO [dbo].[Reviews] ([ReviewId], [AlbumID], [SongID], [PlaylistID], [ReviewType], [UserID], [Rating], [COMMENT]) VALUES (4, NULL, 1, NULL, N'SONG', 1, 5, N'Perfect')
+SET IDENTITY_INSERT [dbo].[Reviews] OFF
+
+
+SET IDENTITY_INSERT [dbo].[Purchase] ON
+INSERT INTO [dbo].[Purchase] ([TransactionID], [UserID], [NumberOfItems], [Amount], [DateOfPurchase]) VALUES (1, 1, 2, CAST(50.0000 AS SmallMoney), N'2013-12-12')
+INSERT INTO [dbo].[Purchase] ([TransactionID], [UserID], [NumberOfItems], [Amount], [DateOfPurchase]) VALUES (2, 5, 3, CAST(100.0000 AS SmallMoney),N'2014-05-05')
+SET IDENTITY_INSERT [dbo].[Purchase] OFF
+
+
+INSERT INTO [dbo].[PurchaseDetails] ([TransactionID], [PurchaseType], [PurchaseItemID], [ItemAmount]) VALUES (1, N'ALBUM', 1, CAST(10.0000 AS SmallMoney))
+INSERT INTO [dbo].[PurchaseDetails] ([TransactionID], [PurchaseType], [PurchaseItemID], [ItemAmount]) VALUES (1, N'SONG', 1, CAST(2.0000 AS SmallMoney))
+INSERT INTO [dbo].[PurchaseDetails] ([TransactionID], [PurchaseType], [PurchaseItemID], [ItemAmount]) VALUES (1, N'ALBUM', 2, CAST(10.0000 AS SmallMoney))
+
